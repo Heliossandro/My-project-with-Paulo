3d01847b6bb1dccbeaa77a9fceac415283dca21f
RandomVfx <zenosama892@gmail.com>
2023-04-04 23:17:43 +0100

    a

--- V12-Gym_Front/src/App.jsx
+++ V12-Gym_Front/src/App.jsx
@@ -1,12 +1,21 @@
 import { Login } from "./components/login/login/Login"
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { CadastroGerente } from "./components/login/cadastroDeGerentes/CadastroGerente"
+import { HourglassMedium } from "phosphor-react"
+import { home } from "./components/home/home"
+
 
 function App() {
 
   return (
-    <BrowserRouter>
-      
+   
+    <home/>
+  )
+}
+
+export default App
+{/* <BrowserRouter>
+       
       <Routes>
         <Route path='/' element={<Login/>}/>
         <Route path="/cadastroGerente" element={<CadastroGerente/>}/>
@@ -16,10 +25,6 @@ function App() {
         <Route path= '/' element={<CadastroGerente/>}/>
         <Route path= '/Login' element={<Login/>}/>
       </Routes>
+      
          
-    </BrowserRouter>
-    
-  )
-}
-
-export default App
+    </BrowserRouter>*/}
