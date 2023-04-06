4635b6cf5ef87627185f197d763f03ae43403f5f
RandomVfx <zenosama892@gmail.com>
2023-04-06 04:08:42 +0100

    a

--- V12-Gym_Front/src/components/pages/home/Home.jsx
+++ V12-Gym_Front/src/components/pages/home/Home.jsx
@@ -6,16 +6,26 @@ import * as S from './styleHome'
 function Home(){
     return(
         <S.container>
-
+            
             <S.sideBar>
-        paulo
+             
             </S.sideBar>
             
             <S.containerContent>
-                <S.header>
-                     <FaFacebook/>
-
-                </S.header>
+            <S.header>
+                <S.navbar class="navbar">
+                     <ul class='links'> 
+                        <li><a href="a">Atletas</a></li>
+                        <li><a href="s">Loja</a></li>
+                        <li><a href="d">Preçarios</a></li>
+                        <li><a href="f">Horario</a></li>
+                        <li><a href="g">Planos</a></li>
+                        <li><a href="g">Estatisticas</a></li>
+                        <li><a href="g">Documentos</a></li>
+                     </ul>
+                </S.navbar>
+            </S.header>
+                
 
                 <S.content>
 
@@ -23,6 +33,7 @@ function Home(){
             </S.containerContent>
 
         </S.container>
+        
     )
 }
 
--- V12-Gym_Front/src/components/pages/home/styleHome.jsx
+++ V12-Gym_Front/src/components/pages/home/styleHome.jsx
@@ -1,11 +1,16 @@
 import styled from "styled-components";
 
 
-export const container = styled.main`
+export const container = styled.main` 
+    margin: 0;
+    padding: 0;
+    background-color: #ccc;
+    box-sizing: border-box;
+    height: 10vh;
 
 `
 export const sideBar = styled.section`
-
+    
 `
 export const containerContent = styled.section`
 
@@ -13,8 +18,41 @@ export const containerContent = styled.section`
 
 export const header = styled.header`
 
+li{
+    list-style: none;
+}
+a:hover{
+color: white;
+}
+a{
+    color: black;
+    text-decoration: none; 
+    position: relative;
+    padding: 0 2rem;
+    display: flex;
+    gap: 2rem;
+}
+
+`
+export const navbar= styled.div`
+
+width:100%
+height: 60px
+max-width: 1200px
+margin: 0 auto;
+display: flex;
+align-items: center;
+justify-content: space-between;
+
+.navbar .links {
+    display: flex;
+    gap:2rem
+}
+
 `
 
+
 export const content = styled.div`
+
 
 `
